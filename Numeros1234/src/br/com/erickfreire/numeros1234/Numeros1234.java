a5b4767ed88e2ad027d0fa0831e6cb84a5473010
Erick Freire <erilefr@gmail.com>
2022-10-08 19:08:15 -0300

    Version 1
    Version 1

---
+++ Numeros1234/src/br/com/erickfreire/numeros1234/Numeros1234.java
@@ -0,0 +1,12 @@
+package br.com.erickfreire.numeros1234;
+
+public class Numeros1234 {
+
+	public static void main(String[] args) {
+		System.out.print("12 34\n");
+		System.out.println("12 34");
+		System.out.printf("12 34%n");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Numeros1234_
  Programa que muestra los números 12 34
+
+## Image
+![Currículo no Computador](numeros1234.png)
